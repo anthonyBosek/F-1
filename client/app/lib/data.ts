9e4ab42c47d387a1a4de8f41a9e4a89f56576f3d
Anthony Bosek <anthony.bosek@gmail.com>
2025-03-25 10:33:23 -0700

    work on definitions.ts
    Co-authored-by: Anthony Bosek <abosek@perseverenow.org>

--- client/app/lib/data.ts
+++ client/app/lib/data.ts
@@ -1,1 +1,45 @@
-// data.ts
+import axios from "axios";
+
+const starWarsAPI = axios.create({
+  baseURL: "https://sw-api.starnavi.io",
+});
+
+export const fetchHeroes = async (page = 1) => {
+  try {
+    const response = await starWarsAPI.get(`/people/?page=${page}`);
+    return response.data;
+  } catch (error) {
+    console.error("Database Error:", error);
+    throw new Error("Failed to fetch heroes data.");
+  }
+};
+
+export const fetchHeroById = async (heroId: number) => {
+  try {
+    const response = await starWarsAPI.get(`/people/${heroId}`);
+    return response.data;
+  } catch (error) {
+    console.error("Database Error:", error);
+    throw new Error("Failed to fetch hero data.");
+  }
+};
+
+export const fetchFilmsByHeroId = async (heroId: number) => {
+  try {
+    const response = await starWarsAPI.get(`/films/?characters=${heroId}`);
+    return response.data;
+  } catch (error) {
+    console.error("Database Error:", error);
+    throw new Error("Failed to fetch films data.");
+  }
+};
+
+export const fetchStarshipsByHeroId = async (heroId: number) => {
+  try {
+    const response = await starWarsAPI.get(`/starships/?pilots=${heroId}`);
+    return response.data;
+  } catch (error) {
+    console.error("Database Error:", error);
+    throw new Error("Failed to fetch starships data.");
+  }
+};
