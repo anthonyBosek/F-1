e1fd6cf9abf6973ef0c53b0828ba3caceb5ef60a
Anthony Bosek <anthony.bosek@gmail.com>
2025-03-26 09:36:19 -0700

    python tests
    Co-authored-by: Anthony Bosek <abosek@perseverenow.org>

--- server/app.py
+++ server/app.py
@@ -1,4 +1,11 @@
 # 
 if __name__ == "__main__":
-    pass
+    cap1 = 'Bangalore'
+    cap2 = 'Londres'
+    cap3 = 'Luanda'
+    cap1 = cap2
+    cap2 = cap3
+    cap3 = cap2
+    cap2 += " " + cap1
+    print(cap2) # Luanda Londres
 # 
