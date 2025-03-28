9e4fdabf533adc924683dc368a3bf86b5b5e7d5b
Anthony Bosek <anthony.bosek@gmail.com>
2025-03-28 08:57:17 -0700

    modifies app in server
    Co-authored-by: Anthony Bosek <abosek@perseverenow.org>

--- server/app.py
+++ server/app.py
@@ -1,13 +1,13 @@
-# 
-if __name__ == "__main__":
-    str1 = 'one'
-    str2 = 'two'
-    str3 = 'three'
-    str1 = str2
-    str2 = str3
-    str3 = str2
-    str2 += " " + str1
-    print(str2) # three two
-    print(str3) # three
-    print(str1) # two
+# nonsense practice code
+# if __name__ == "__main__":
+#     str1 = 'one'
+#     str2 = 'two'
+#     str3 = 'three'
+#     str1 = str2
+#     str2 = str3
+#     str3 = str2
+#     str2 += " " + str1
+#     print(str2) # three two
+#     print(str3) # three
+#     print(str1) # two
 # 
